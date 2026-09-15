8a2c1c559230691155a56739ffd4b53314ae2d4c
beloyarova <abeloarova@gmail.com>
2026-09-15 11:09:53 +0300

    Lab04: if and else, age categories

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Привет");
+﻿int age = 15;
+if (age >= 21)
+{
+    Console.WriteLine("Доступ разрешен");
+}
+Console.WriteLine("Программа продолжает работу");
+
+int age1 = 15;
+if (age >= 18)
+{
+    Console.WriteLine("Доступ разреен");
+}else
+{
+
+    Console.WriteLine("Доступ запрещен");
+    Console.WriteLine($"До совершеннолетия осталось: {18 - age1} года");
+}
